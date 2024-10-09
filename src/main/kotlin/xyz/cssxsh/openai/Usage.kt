5b43c83e0dbb050cb2f6d9bc2503d30941e93c94
zyf <realzyf@gmail.com>
2024-10-09 20:48:28 +0800

    适配api改动：ChatInfo下的Usage下新增completion_tokens_details字段和prompt_tokens_details字段

--- src/main/kotlin/xyz/cssxsh/openai/Usage.kt
+++ src/main/kotlin/xyz/cssxsh/openai/Usage.kt
@@ -9,5 +9,21 @@ public data class Usage(
     @SerialName("prompt_tokens")
     val promptTokens: Int = 0,
     @SerialName("total_tokens")
-    val totalTokens: Int = 0
+    val totalTokens: Int = 0,
+    @SerialName("completion_tokens_details")
+    val completion_tokens_details: CompletionTokensDetails? = null,
+    @SerialName("prompt_tokens_details")
+    val prompt_tokens_details: PromptTokensDetails? = null
+)
+
+@Serializable
+public data class CompletionTokensDetails(
+    @SerialName("reasoning_tokens")
+    val reasoning_tokens: Int = 0
+)
+
+@Serializable
+public data class PromptTokensDetails(
+    @SerialName("cached_tokens")
+    val cached_tokens: Int = 0
 )
